b14d501afd3f6e3a000e234d56c2b4a2865d8b07
haoyuheng <haoyuheng07@gmail.com>
2014-11-25 22:43:18 +0800

    Signed-off-by: haoyuheng <haoyuheng07@gmail.com>

---
+++ src/mobisocial/musubi/test/JsonGernerator.java
@@ -0,0 +1,34 @@
+package mobisocial.musubi.test;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Base64;
+import android.util.Log;
+
+/*
+ * by haoyuheng
+ * JsonGernerator is class to gernerate the sig_key and the cypto_key
+ * instead of get if from the server
+ * here just used to test.
+ */
+
+
+public class JsonGernerator {
+
+	public static final String TAG = "JsonGernerator";
+	public static JSONObject GerneratorAphidResult(long time){
+		JSONObject jsonObj = new JSONObject();
+		try {
+			jsonObj.put("sig_key", Base64.encodeToString("sig_key1234".getBytes(),Base64.DEFAULT));
+			jsonObj.put("crypto_key", Base64.encodeToString("crypto_key1234".getBytes(),Base64.DEFAULT));
+			jsonObj.put("time", time);
+		} catch (JSONException e) {
+			Log.e(TAG, e.toString());
+		}
+		return jsonObj;
+	}
+	
+}
